1e3bae43ca40f007f40ec269a35df6140ae4abf3
Chris Tarazi <chris.tarazi@yahoo.com>
2015-04-25 20:06:11 -0700

    Completed check for var declarations

--- Part2/2.cpp
+++ Part2/2.cpp
@@ -4,12 +4,34 @@
 #include <fstream>
 #include <string>
 #include <cctype>
-#include <list>
+#include <vector>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
-void detectKeywords(list<string> l)
+// Function to check if string is a valid variable name.
+bool isIdentifier(string s)
+{
+	if (s.compare("integer") == 0 || s.compare("begin") == 0 ||
+		s.compare("end") == 0 || s.compare("var") == 0)
+	{
+		return false;
+	}
+	if (s[0] == '_' || isalpha(s[0]))
+	{
+		int space = s.find(" ");
+		if (space == -1)
+		{
+			return true;
+		}
+		else return false;
+	}
+	else return false;
+}
+
+// Function to detect certain mandatory keywords.
+void detectKeywords(vector<string> l)
 {
 	string temp = "";
 	for (auto&& i : l.front())
@@ -37,9 +59,63 @@ void detectKeywords(list<string> l)
 	if (!endWord) cout << "end. is expected \n";
 }
 
+// Function to check the line after the "var" line in text file.
+void ensureVarDeclarations(vector<string> l)
+{
+	int index = 0;
+	for (auto&& i : l)
+	{
+		index++;
+		if (i.compare("var") == 0)
+		{
+			break;
+		}
+	}
+
+	string temp = l[index];
+	int integerKeyword = temp.find("integer");
+
+	stringstream ss(temp);	// tokenize temp string
+	string token;
+	vector<string> v;
+	while(ss >> token) v.push_back(token);	// insert each token in a vector
+
+	if (integerKeyword != -1)
+	{
+		int IDs = 0;
+		int commas = 0;
+		int colons = 0;
+		for (int i = 0; i < v.size(); i++)
+		{
+			if (isIdentifier(v[i]))
+			{
+				IDs++;
+			}
+			else if (v[i] == ",")
+			{
+				commas++;
+			}
+			else if (v[i] == ":")
+			{
+				colons++;
+			}
+		}
+
+		if (IDs - commas > 1) cout << "more than one comma is missing\n";
+		else if (IDs - commas < 1 && (IDs - commas) != 1) cout << "too many commas\n";
+
+		if (colons != 1) cout << "colon is missing\n";
+
+		if (v[v.size()-1] != ";")
+		{
+			cout << "semicolon is missing\n";
+		}
+	}	
+}
+
 int main()
 {
-	list<string> l;
+	vector<string> l;
 	fstream fIn("finalp2.txt", ios::in);
 	string line;
 
@@ -47,10 +123,10 @@ int main()
 	{
 		l.push_back(line);
 	}
-
 	fIn.close();
 
 	detectKeywords(l);
+	ensureVarDeclarations(l);
 
 	return 0;
 }
